3c846f1665b92492a07e710fefc322f048a1e2df
Alistair Sykes <alistair@brightec.co.uk>
2019-05-09 09:45:59 +0100

    master. Updated various versions of dependencies. Updated the quality config files.

--- app/src/main/java/uk/co/brightec/kbarcode/app/viewfinder/Resource.kt
+++ app/src/main/java/uk/co/brightec/kbarcode/app/viewfinder/Resource.kt
@@ -3,8 +3,8 @@ package uk.co.brightec.kbarcode.app.viewfinder
 /**
  * A generic class that describes some data with a status
  * Inspired by:
- * [Architecture Guide](https://developer.android.com/topic/libraries/architecture/guide.html#addendum)
- * [Github](https://github.com/googlesamples/android-architecture-components/tree/master/GithubBrowserSample)
+ * https://developer.android.com/topic/libraries/architecture/guide.html#addendum
+ * https://github.com/googlesamples/android-architecture-components/tree/master/GithubBrowserSample
  */
 @Suppress("unused")
 sealed class Resource<T> {
